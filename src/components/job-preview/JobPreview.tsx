035256984fd7553dc03673e228748063083bf7d3
Barabaka <ivan.gopey@gmail.com>
2022-11-01 00:15:16 +0200

    Update some styles

--- src/components/job-preview/JobPreview.tsx
+++ src/components/job-preview/JobPreview.tsx
@@ -2,6 +2,9 @@ import {
   JobPreviewWrapper,
   CompanyLogoSection,
   Logo,
+  JobDescriptionWrapper,
+  TextDark,
+  TextSecondary,
 } from "./JobPreviewStyles";
 
 interface jobPreviewProps {
@@ -21,9 +24,11 @@ export const JobPreview = ({ jobPreviewData }: jobPreviewProps) => {
       <CompanyLogoSection>
         <Logo src={jobPreviewData.logo} alt="Company Logo" />
       </CompanyLogoSection>
-      <div className="jobDescription">
-        {jobPreviewData.title} {jobPreviewData.jobName} {jobPreviewData.address}
-      </div>
+      <JobDescriptionWrapper>
+        <TextDark>{jobPreviewData.title}</TextDark>
+        <TextSecondary>{jobPreviewData.jobName}</TextSecondary>
+        <TextSecondary>{jobPreviewData.address}</TextSecondary>
+      </JobDescriptionWrapper>
       <div className="rates"></div>
       <div className="save date">{jobPreviewData.date}</div>
     </JobPreviewWrapper>
--- src/components/job-preview/JobPreviewStyles.tsx
+++ src/components/job-preview/JobPreviewStyles.tsx
@@ -2,9 +2,9 @@ import styled from "@emotion/styled";
 import "font-proxima-nova/style.css";
 
 export const JobPreviewWrapper = styled.div`
-  font-family: "Proxima Nova";
+  font-family: "Proxima Nova Rg";
   width: 80%;
-  height: 120px;
+  height: 150px;
   margin: 5px auto;
   display: flex;
   background-color: white;
@@ -13,14 +13,37 @@ export const JobPreviewWrapper = styled.div`
 `;
 
 export const CompanyLogoSection = styled.div`
-  width: 10%;
+  max-width: 13%;
   height: 100%;
 `;
 
 export const Logo = styled.img`
-  height: 60px;
-  width: 60px;
+  height: 85px;
+  width: 85px;
   border-radius: 50%;
-  margin-top: 15px;
+  margin-top: 20px;
   margin-left: 10px;
+`;
+
+export const JobDescriptionWrapper = styled.div`
+  margin-top: 20px;
+  margin-left: 15px;
+  display: flex;
+  flex-direction: column;
+  max-width: 50%;
+`;
+
+export const TextDark = styled.span`
+  font-weight: 700;
+  color: #3a4562;
+  font-size: 20px;
+  letter-spacing: -0.625px;
+`;
+
+export const TextSecondary = styled.span`
+  font-weight: 400;
+  font-size: 16px;
+  letter-spacing: 0.23619px;
+  color: #878d9d;
+  margin-top: 10px;
 `;
